843e3893d56fa6fd367432d112de88d91496ca79
zemahran <zemahran@gmail.com>
2019-03-09 16:01:42 +0200

    Logistic Regression

--- machine-learning-ex2/ex2.m
+++ machine-learning-ex2/ex2.m
@@ -1,20 +1,4 @@
 %% Machine Learning Online Class - Exercise 2: Logistic Regression
-%%
-% 
-%  This file contains code that helps you get started on the logistic
-%  regression exercise. You will need to complete the following functions 
-%  in this exericse:
-%  
-%     sigmoid.m
-%     costFunction.m
-%     predict.m
-%     costFunctionReg.m
-%  
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-% 
-%%
 clear ; close all; clc
 %% Load Data
 %%
